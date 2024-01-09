ae3c85acdad594510cc20ddbd00b24843add5b59
MasaSky <152603719+MasaSky@users.noreply.github.com>
2024-01-09 21:52:53 +0000

    240110*-01

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
@@ -8,16 +9,17 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
 
-  root to: 'homes#top'
 
-  devise_for :users
-
-  get 'homes/about' => 'homes#about', as:'about'
-
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
+
 
 end
